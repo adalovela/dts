4602000ffd3509dd74123e157aa878715679cb5a
Zeus Labrador <zeuslabrador@protonmail.com>
2020-05-04 19:36:48 -0500

    Add Angular/Node with a Dockerfile via port 4200
        * Install and use APM Angular package

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CoreModule } from '@core';
+import { SharedModule } from '@shared';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { ErrorHandler } from '@angular/core';
+import { ApmErrorHandler } from '@elastic/apm-rum-angular';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    NgbModule,
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    AppRoutingModule, // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: ApmErrorHandler,
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
