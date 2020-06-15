1c7c6b492efbf5b76ccf860abf94210b28787c08
Ada Lovelace <ada@lovelace.com>
2020-06-15 12:53:52 +0400

    Fixes wrong resolution of namespace

--- k8s/delete_all.sh
+++ k8s/delete_all.sh
@@ -9,49 +9,87 @@ kubectl delete -f ./app/05-spanish.yaml
 helm uninstall dts-cp-kafka 
 
 ############## Istio ##############
-# Grafana
-kubectl delete -f istio/grafana/istio-grafana-citadel-dashboard.yaml
-kubectl delete -f istio/grafana/istio-grafana-custom-resources-config.yaml
-kubectl delete -f istio/grafana/istio-grafana-galley-dashboard.yaml
-kubectl delete -f istio/grafana/istio-grafana-mesh-dashboard.yaml
-kubectl delete -f istio/grafana/istio-grafana-mixer-dashboard.yaml
-kubectl delete -f istio/grafana/istio-grafana-perf-dashboard.yaml
-kubectl delete -f istio/grafana/istio-grafana-pilot-dashboard.yaml
-kubectl delete -f istio/grafana/istio-grafana-service-dashboard.yaml
-kubectl delete -f istio/grafana/istio-grafana-workload-dashboard.yaml
+# Destination Rules
+kubectl delete -f 01-istio/17-destinationrules
 
-# Cluster Roles
-kubectl delete -f istio/clusterroles
+# Rules
+kubectl delete -f 01-istio/16-rules
 
-# Roles
-kubectl delete -f istio/roles
+# Instances
+kubectl delete -f 01-istio/15-instances
 
-# Services
-kubectl delete -f istio/services
+# Handlers
+kubectl delete -f 01-istio/14-handlers
 
-# Deployments
-kubectl delete -f istio/deployments
-
-# Lists
-kubectl delete -f istio/lists
+# Attribute Manifests
+kubectl delete -f 01-istio/13-attributemanifests
 
 # Pod Disruption Budgets
-kubectl delete -f istio/poddisruption
+kubectl delete -f 01-istio/12-poddisruptions
 
-# Custom Resource Definitions
-kubectl delete -f istio/customresourcedefinitions
+# Mutating Webhooks config
+kubectl delete -f 01-istio/11-mutatingwebhooks
+
+# Lists
+kubectl delete -f 01-istio/10-lists
+
+# Deployments
+kubectl delete -f 01-istio/09-deployments
+
+# Services
+kubectl delete -f 01-istio/08-services
+
+# Roles
+kubectl delete -f 01-istio/07-roles
+
+# Jobs
+kubectl delete -f 01-istio/06-jobs
+
+# Cluster Roles
+kubectl delete -f 01-istio/05-clusterroles
 
 # Service Accounts
-kubectl delete -f istio/serviceaccounts
+kubectl delete -f 01-istio/04-serviceaccounts
+
+# Grafana
+kubectl delete -f 01-istio/03-grafana/istio-grafana-citadel-dashboard.yaml
+kubectl delete -f 01-istio/03-grafana/istio-grafana-custom-resources-config.yaml
+kubectl delete -f 01-istio/03-grafana/istio-grafana-galley-dashboard.yaml
+kubectl delete -f 01-istio/03-grafana/istio-grafana-mesh-dashboard.yaml
+kubectl delete -f 01-istio/03-grafana/istio-grafana-mixer-dashboard.yaml
+kubectl delete -f 01-istio/03-grafana/istio-grafana-perf-dashboard.yaml
+kubectl delete -f 01-istio/03-grafana/istio-grafana-pilot-dashboard.yaml
+kubectl delete -f 01-istio/03-grafana/istio-grafana-service-dashboard.yaml
+kubectl delete -f 01-istio/03-grafana/istio-grafana-workload-dashboard.yaml
 
 # Config Maps
-kubectl delete -f istio/config
+kubectl delete -f 01-istio/02-config
 
 # Misc
-kubectl delete -f istio/
+kubectl delete -f 01-istio/
+
+# Secrets
+kubectl delete -f 01-istio/01-secrets
+
+# Custom Resource Definitions
+kubectl delete -f 01-istio/00-customresourcedefinitions
 
 ############## Namespace ##############
 kubectl delete -f ./namespace.yaml
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
