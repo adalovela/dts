3f5d333ea5cc8336c7710c497b8190e8073ae1fd
Ada Lovelace <ada@lovelace.com>
2020-06-15 11:02:31 +0400

    Reorganizes istio yaml files to respect order

--- k8s/apply_all.sh
+++ k8s/apply_all.sh
@@ -3,62 +3,78 @@
 kubectl apply -f ./namespace.yaml
 
 ############## Istio ##############
-# Secrets
-kubectl apply -f istio/01-secrets
-
-# Misc
-kubectl apply -f istio/
-
-# Config Maps
-kubectl apply -f istio/02-config
-
-# Grafana
-kubectl apply -f istio/03-grafana/istio-grafana-citadel-dashboard.yaml
-kubectl apply -f istio/03-grafana/istio-grafana-custom-resources-config.yaml
-kubectl apply -f istio/03-grafana/istio-grafana-galley-dashboard.yaml
-kubectl apply -f istio/03-grafana/istio-grafana-mesh-dashboard.yaml
-kubectl apply -f istio/03-grafana/istio-grafana-mixer-dashboard.yaml
-kubectl apply -f istio/03-grafana/istio-grafana-perf-dashboard.yaml
-kubectl apply -f istio/03-grafana/istio-grafana-pilot-dashboard.yaml
-kubectl apply -f istio/03-grafana/istio-grafana-service-dashboard.yaml
-kubectl apply -f istio/03-grafana/istio-grafana-workload-dashboard.yaml
-
-# Service Accounts
-kubectl apply -f istio/04-serviceaccounts
-
-# Cluster Roles
-kubectl apply -f istio/05-clusterroles
-
-# Jobs
-kubectl apply -f istio/06-jobs
-
-# Roles
-kubectl apply -f istio/07-roles
-
-# Services
-kubectl apply -f istio/08-services
-
-# Deployments
-kubectl apply -f istio/09-deployments
-
-# Lists
-kubectl apply -f istio/10-lists
-
-# Mutating Webhooks config
-kubectl apply -f istio/11-mutatingwebhooks
-
-# Pod Disruption Budgets
-kubectl apply -f istio/12-poddisruption
 
 # Custom Resource Definitions
-kubectl apply -f istio/customresourcedefinitions
+kubectl apply -f 01-istio/00-customresourcedefinitions
+
+# Secrets
+kubectl apply -f 01-istio/01-secrets
+
+# Misc
+kubectl apply -f 01-istio/
+
+# Config Maps
+kubectl apply -f 01-istio/02-config
+
+# Grafana
+kubectl apply -f 01-istio/03-grafana/istio-grafana-citadel-dashboard.yaml
+kubectl apply -f 01-istio/03-grafana/istio-grafana-custom-resources-config.yaml
+kubectl apply -f 01-istio/03-grafana/istio-grafana-galley-dashboard.yaml
+kubectl apply -f 01-istio/03-grafana/istio-grafana-mesh-dashboard.yaml
+kubectl apply -f 01-istio/03-grafana/istio-grafana-mixer-dashboard.yaml
+kubectl apply -f 01-istio/03-grafana/istio-grafana-perf-dashboard.yaml
+kubectl apply -f 01-istio/03-grafana/istio-grafana-pilot-dashboard.yaml
+kubectl apply -f 01-istio/03-grafana/istio-grafana-service-dashboard.yaml
+kubectl apply -f 01-istio/03-grafana/istio-grafana-workload-dashboard.yaml
+
+# Service Accounts
+kubectl apply -f 01-istio/04-serviceaccounts
+
+# Cluster Roles
+kubectl apply -f 01-istio/05-clusterroles
+
+# Jobs
+kubectl apply -f 01-istio/06-jobs
+
+# Roles
+kubectl apply -f 01-istio/07-roles
+
+# Services
+kubectl apply -f 01-istio/08-services
+
+# Deployments
+kubectl apply -f 01-istio/09-deployments
+
+# Lists
+kubectl apply -f 01-istio/10-lists
+
+# Mutating Webhooks config
+kubectl apply -f 01-istio/11-mutatingwebhooks
+
+# Pod Disruption Budgets
+kubectl apply -f 01-istio/12-poddisruptions
+
+# Attribute Manifests
+kubectl apply -f 01-istio/13-attributemanifests
+
+# Handlers
+kubectl apply -f 01-istio/14-handlers
+
+# Instances
+kubectl apply -f 01-istio/15-instances
+
+# Rules
+kubectl apply -f 01-istio/16-rules
+
+# Destination Rules
+kubectl apply -f 01-istio/17-destinationrules
 
 ############## Kafka ##############
-helm install dts-cp-kafka ./kafka/charts/
+helm install dts-cp-kafka ./02-kafka/charts/
 
 ############## Dummy Translator Service ##############
-kubectl apply -f ./app/02-configs.yaml
-kubectl apply -f ./app/03-translator.yaml
-kubectl apply -f ./app/04-english.yaml
-kubectl apply -f ./app/05-spanish.yaml
+kubectl apply -f ./03-app/02-configs.yaml
+kubectl apply -f ./03-app/03-translator.yaml
+kubectl apply -f ./03-app/04-english.yaml
+kubectl apply -f ./03-app/05-spanish.yaml
 
